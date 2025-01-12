ef5636f9ddda0c5a88e4eb0b788643929844c441
jeonggiju <halogiju123@gmail.com>
2025-01-13 01:10:56 +0900

    2025/1/13

---
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,47 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+* */
+public class MemberRepository {
+    /**
+     * static 이기에 객체가 여럿 생성되어도 하나만 생성된다.
+    * */
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    private MemberRepository() {}
+
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(long id) {
+        return store.get(id);
+    }
+
+    /**
+     * [Member{id=1, name='Alice'}, Member{id=2, name='Bob'}, Member{id=3, name='Charlie'}]
+    * */
+    public List<Member> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
